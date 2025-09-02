a5d26ed784ff4f5c9bdfa70c42779fe93b6120d4
ogzacky75 <ogzacky75@gmail.com>
2025-09-02 19:44:24 +0300

    fixing book.py errors

--- lib/book.py
+++ lib/book.py
@@ -3,7 +3,7 @@
 class Book:
     def __init__(self, title, page_count):
         self.title = title
-        self.page_count = page_count  # triggers setter validation
+        self.page_count = page_count
 
     @property
     def page_count(self):
